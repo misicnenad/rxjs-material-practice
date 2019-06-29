4723397d2453d4bfdaca055fd3064e212e7a7947
shonmisic <19427053+shonmisic@users.noreply.github.com>
2019-06-29 21:13:56 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rmp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rxjs-material-practice';
+}
